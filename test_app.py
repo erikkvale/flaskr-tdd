afef3aa559b18d4b552234f13f9f7601ed703b14
Erik Kvale <eirikval@gmail.com>
2018-07-15 11:47:02 -0500

    Updated .gitignore to ignore sqlte db, modified old tests for BasicTestCase class, fail on test_index, pass on test_database, added docstrings to both methods

--- test_app.py
+++ test_app.py
@@ -6,11 +6,17 @@ from app import app
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
+        """
+        Initial test, ensures Flask was setup properly
+        """
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
     def test_database(self):
+        """
+        Initial test, ensures the database was setup
+        """
         tester = os.path.exists('flaskr.db')
         self.assertTrue(tester)
 
