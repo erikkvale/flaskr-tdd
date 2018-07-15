b2a648cfddb133d012ebef83575bb2aa6105f3fb
Erik Kvale <eirikval@gmail.com>
2018-07-15 12:11:02 -0500

    Added new TestCase, setup and teardown methods, as well as login and logout functions

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 import unittest
 import os
-from app import app
+import tempfile
+import app
 
 
 class BasicTestCase(unittest.TestCase):
@@ -9,7 +10,7 @@ class BasicTestCase(unittest.TestCase):
         """
         Initial test, ensures Flask was setup properly
         """
-        tester = app.test_client(self)
+        tester = app.app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
@@ -19,6 +20,50 @@ class BasicTestCase(unittest.TestCase):
         """
         tester = os.path.exists('flaskr.db')
         self.assertTrue(tester)
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Set up a blank temp database before each test
+        """
+        self.db_file_desc, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+        app.init_db()
+
+    def tearDown(self):
+        """
+        Destroy blank temp database after each test
+        """
+        os.close(self.db_file_desc)
+        os.unlink(app.app.config['DATABASE'])
+
+
+    def login(self, username, password):
+        """
+        Login helper function
+        """
+        return self.app.post(
+            '/login',
+            data=dict(
+                username=username,
+                password=password
+            ),
+            follow_redirects=True
+        )
+
+    def logout(self):
+        """
+        Logout helper function
+        """
+        return self.app.get('/logout', follow_redirects=True)
+
+
+
+
+
 
 if __name__=='__main__':
     unittest.main()
