9692f66cc2c0f53c2035ea9eb36bd9365f429cc7
Erik Kvale <eirikval@gmail.com>
2018-07-15 11:37:27 -0500

    Updated app code with database initialization code and altered 'public' functions to 'private' implementations with leading underscores, this diverges from the tutorial, will make them public if needed, but otherwise they should not be accessed. Tests passed!

--- app.py
+++ app.py
@@ -17,5 +17,44 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
+def init_db():
+    """
+    Create the database
+    """
+    with app.app_context():
+        db = _open_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def _connect_db():
+    """
+    Connects to the database
+    """
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def _open_db():
+    """
+    Open database connnection
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = _connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def _close_db(error):
+    """
+    Close database connection
+    """
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
 if __name__=='__main__':
+    init_db()
     app.run()
