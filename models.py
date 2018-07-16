a692e2497e959646145a2f70f71a66049d32fb27
Erik Kvale <eirikval@gmail.com>
2018-07-16 13:28:02 -0500

    Created model

--- models.py
+++ models.py
@@ -2,4 +2,15 @@ from app import db
 
 
 class Flaskr(db.Model):
-    pass
+    __tablename__='flaskr'
+
+    post_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    text = db.Column(db.String, nullable=False)
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+
+    def __repr__(self):
+        return '<title {}>'.format(self.title)
